e6862e4d0f1acf676fb1aab2dbed1a809bedd9ce
cybulskm <99496865+cybulskm@users.noreply.github.com>
2023-12-27 22:32:11 -0500

    added spawn all items patch

--- Patches/StartOfRoundPatch.cs
+++ Patches/StartOfRoundPatch.cs
@@ -1,12 +1,27 @@
-﻿using System;
+﻿using HarmonyLib;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace AdminMode.Patches
 {
+    [HarmonyPatch(typeof(StartOfRound))]
+
     internal class StartOfRoundPatch
     {
+        [HarmonyPatch("LoadUnlockables")]
+        [HarmonyPrefix]
+        static void SpawnShotGun(ref List<UnlockableItem> ___unlockables)
+        {
+            for (int i = 0; i< ___unlockables.Count; i++)
+            {
+                ___unlockables[i].hasBeenUnlockedByPlayer = true;
+                Debug.WriteLine(___unlockables[i].unlockableName);
+            }
+        }
+
     }
 }
